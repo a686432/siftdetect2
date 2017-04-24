376b4e9dc1aae10dce109add44b47472e9a928b1
jdq <490396342@qq.com>
2017-04-24 15:37:08 +0800

    a

--- test.py
+++ test.py
@@ -2,56 +2,102 @@ import cv2
 import method
 import numpy as np
 
-weigh=np.load("a1000.npy")
-distinguish=np.load("c1000.npy")
-img=cv2.imread("2.jpg")
-appearance=np.load("d1000.npy")
-img=cv2.resize(img,(1000,1000))
-matchs=[]
-sift = cv2.xfeatures2d.SIFT_create(1000)
-kp, res = sift.detectAndCompute(img, None)
-appearance_sort=sorted(appearance,reverse=True)
-res0=[]
-responses=[]
-th=appearance_sort[100]
-out=img
-for i in range(0,len(kp)):
-    if appearance[i]>=th:
-        out = cv2.circle(out, (int(kp[i].pt[0]),int(kp[i].pt[1])), 2, (55, 255, 155), 2)
-        res0.append(res[i])
-        responses.append(i)
-responses=np.array(responses)
-res0=np.array(res0)
-print responses
-cv2.imshow("strong sift descriptor", out)
-cv2.waitKey(0)
 
-sift = cv2.xfeatures2d.SIFT_create(1000)
-img2, M = method.dataIncreasing_camera(img)
-img2=cv2.imread("data/40103/1.jpg")
-img2=cv2.resize(img2,(1000,1000))
-kp2, res2 = sift.detectAndCompute(img2, None)
+def inputimage():
+    basefilename = "res/40108/"
+    inputfilename = "data/40108/"
+    img = cv2.imread(inputfilename + "0.jpg")
+    img2, M = method.dataIncreasing_camera(img)
+    #img2 = cv2.imread(inputfilename + "1.jpg")
+    img2 = cv2.resize(img2, (1000, 1000))
+    weigh = np.load(basefilename + 'a' + str(nfeature) + '.npy')
+    distinguish = np.load(basefilename + 'c' + str(nfeature) + '.npy')
+    appearance = np.load(basefilename + 'd' + str(nfeature) + '.npy')
+    img = cv2.resize(img, (1000, 1000))
+    return weigh, distinguish, appearance, img, img2
 
-# responses = np.array([k for k in range(0, len(kp))])
-# print responses
 
-knn = cv2.ml.KNearest_create()
-knn.train(res0, cv2.ml.ROW_SAMPLE, responses)
-out=img
-# for i in range(0,len(kp)):
-#     out = cv2.circle(img, (int(kp[i].pt[0]),int(kp[i].pt[1])), int(20*distinguish[0,i]), (255, 255, int(255*distinguish[1,i])), 1)
-#     out = cv2.circle(img, (int(kp[i].pt[0]), int(kp[i].pt[1])), int(20 * distinguish[1, i]),
-#                      (255, 0, 255), 1)
-ret, results, neighbours, dist = knn.findNearest(res2, 3)
-for i in range(0, len(neighbours)):
-    print "Match:"+str(dist[i])+"Weigh:"+str(weigh[int(neighbours[i][0])])+"Num"+str(int(neighbours[i][0]))
-    if weigh[int(neighbours[i][0]),0]<2000000:
-        if dist[i][0]<(weigh[int(neighbours[i][0]), 0]+weigh[int(neighbours[i][0]), 1])/2 and distinguish[0,int(neighbours[i][0])]>0:
+def siftKnn():
+    sift = cv2.xfeatures2d.SIFT_create(1000)
+    kp, res = sift.detectAndCompute(img, None)
+    sift = cv2.xfeatures2d.SIFT_create(1000)
+    kp2, res2 = sift.detectAndCompute(img2, None)
+    return kp, res,kp2, res2
+
+
+
+if __name__ == "__main__":
+    MIN_MATCH_COUNT = 5
+    nfeature = 1000
+    npoint = 100
+
+    weigh, distinguish, appearance, img, img2 = inputimage()
+    kp, res, kp2, res2 = siftKnn()
+    matchs = []
+    appearance_sort = sorted(appearance, reverse=True)
+    res0 = []
+    responses = []
+    transform = []
+    th = appearance_sort[npoint]
+    th=0
+    lists = []
+    out = img
+    knn = cv2.ml.KNearest_create()
+
+
+    m = np.zeros(len(kp))
+    # for i in range(0, len(kp)):
+    #     k = 0
+    #     if appearance[i] >= th:
+    #         out = cv2.circle(out, (int(kp[i].pt[0]), int(kp[i].pt[1])), 2, (55, 255, 155), 2)
+    #         res0.append(res[i])
+    #         responses.append(i)
+    #         m[i] = k
+    #         k += 1
+    #         a = []
+    #         lists.append(a)
+    # responses = np.array(responses)
+    # res0 = np.array(res0)
+    # print responses
+    # cv2.imshow("strong sift descriptor", out)
+    knn.train(res, cv2.ml.ROW_SAMPLE, responses)
+    out = img
+    ret, results, neighbours, dist = knn.findNearest(res2, 3)
+    print neighbours
+    cv2.waitKey(0)
+    print len(neighbours)
+
+    for i in range(0, len(neighbours)):
+        #print "Match:" + str(dist[i]) + "Weigh:" + str(weigh[int(neighbours[i][0])]) + "Num" + str(
+         #   int(neighbours[i][0]))
+        if dist[i][0] < min(weigh[int(neighbours[i][0]), 1] , weigh[
+            int(neighbours[i][0]), 0]):  # and dist[i][0]/dist[i][1]<distinguish[1,int(neighbours[i][0])]:
             match = cv2.DMatch(int(neighbours[i][0]), i, 3)
             print "Good Match"
             matchs.append(match)
-out = cv2.drawMatches(img, kp, img2, kp2, matchs,out)
-out = cv2.resize(out,(1600,1000))
-cv2.imshow("sift picture of translation", out)
-cv2.waitKey(0)
-print weigh
+            # lists[]
+            print int(neighbours[i][0]),i
+    if len(matchs) > MIN_MATCH_COUNT:
+        src_pts = np.float32([kp[m.queryIdx].pt for m in matchs[:50]]).reshape(-1, 1, 2)
+        dst_pts = np.float32([kp2[m.trainIdx].pt for m in matchs[:50]]).reshape(-1, 1, 2)
+
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+        matchesMask = mask.ravel().tolist()
+
+        if len(img.shape) > 2:
+            h, w, d = img.shape
+            pts = np.float32([[[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]])
+            dst = cv2.perspectiveTransform(pts, M)
+
+            img2 = cv2.polylines(img2, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+
+    else:
+        print "Not enough matches are found - %d/%d" % (len(matchs), MIN_MATCH_COUNT)
+        matchesMask = None
+
+    out = cv2.drawMatches(img, kp, img2, kp2, matchs, out)
+    out = cv2.resize(out, (1600, 1000))
+    cv2.imshow("sift picture of translation", out)
+    cv2.imshow("sift picture of translation2", img2)
+    cv2.waitKey(0)
+    print weigh
--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 import numpy as np
 import method
@@ -33,12 +34,13 @@ def siftComp(img1, nloop):
             (x0, y0) = dst[int(neighbours[k][0])]
             (x1, y1) = kp2[k].pt
             err = np.sqrt((x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0))
-            if err < 10:
-                if distinguish[0,int(neighbours[k][0])]==0 or distinguish[0,int(neighbours[k][0])]<dist[k][0]/dist[k][1]:
-                    distinguish[0,int(neighbours[k][0])]=dist[k][0]/dist[k][1]
+            if err < 3:
+                if distinguish[0, int(neighbours[k][0])] == 0 or distinguish[0, int(neighbours[k][0])] < dist[k][0] / \
+                        dist[k][1]:
+                    distinguish[0, int(neighbours[k][0])] = dist[k][0] / dist[k][1]
                 img2 = cv2.circle(img2, (x0, y0), 6, (255, 155, 55), 1)
-                apprearance[int(neighbours[k][0])]+=1
-                #print "GOOD NO." + str(int(neighbours[k][0])) + "\nthe shortest distence is:" + str(dist[k])
+                apprearance[int(neighbours[k][0])] += 1
+                # print "GOOD NO." + str(int(neighbours[k][0])) + "\nthe shortest distence is:" + str(dist[k])
                 if dist[k][0] < predict[int(neighbours[k][0])]:
                     print "Preduct is True"
                     nTure.append(int(neighbours[k][0]))
@@ -52,9 +54,10 @@ def siftComp(img1, nloop):
                     match = cv2.DMatch(int(neighbours[k][0]), k, 3)
                     matchs.append(match)
             else:
-                if distinguish[1,int(neighbours[k][0])]==0 or distinguish[1,int(neighbours[k][0])]>dist[k][0]/dist[k][1]:
-                    distinguish[1,int(neighbours[k][0])]=dist[k][0]/dist[k][1]
-                #print "BAD NO." + str(int(neighbours[k][0])) + "\nthe shortest distence is:" + str(dist[k])
+                if distinguish[1, int(neighbours[k][0])] == 0 or distinguish[1, int(neighbours[k][0])] > dist[k][0] / \
+                        dist[k][1]:
+                    distinguish[1, int(neighbours[k][0])] = dist[k][0] / dist[k][1]
+                # print "BAD NO." + str(int(neighbours[k][0])) + "\nthe shortest distence is:" + str(dist[k])
                 if dist[k][0] < predict[int(neighbours[k][0])]:
                     print "Preduct is False"
                     nnFalse.append(int(neighbours[k][0]))
@@ -66,35 +69,46 @@ def siftComp(img1, nloop):
                     predict[int(neighbours[k][0])] = min(weigh[int(neighbours[k][0]), 0],
                                                          weigh[int(neighbours[k][0]), 1])
         print apprearance
-        print "The number of true match and false match is " + str((len(nTure), len(nFalse), len(nTure)*1.0 / (len(nFalse)+len(nTure))))
-        print "The number of true match and false match is " + str((len(nTure), len(nnFalse), len(nTure)*1.0 / (len(nnFalse)+len(nTure))))
+        print "The number of true match and false match is " + str(
+            (len(nTure), len(nFalse), len(nTure) * 1.0 / (len(nFalse) + len(nTure))))
+        print "The number of true match and false match is " + str(
+            (len(nTure), len(nnFalse), len(nTure) * 1.0 / (len(nnFalse) + len(nTure))))
 
-        acc+=len(nTure)*1.0 / (len(nnFalse)+len(nTure))
+        acc += len(nTure) * 1.0 / (len(nnFalse) + len(nTure))
 
-
-    print acc/nloop
+    print acc / nloop
     out = cv2.drawMatches(img1, kp, img2, kp2, matchs, out)
     cv2.imshow("sift picture of translation", out)
 
     return
+
+
 '''
 The program achieve the following function: 
 1.Find the robust sift descriptor:
     we use a series of the transforms and record the 
 
 '''
-
 nfeature = 1000
-# weigh = np.zeros((nfeature, 2))
-# distinguish=np.zeros((2,nfeature))
-# predict = np.zeros(nfeature)
-apprearance=np.zeros(nfeature)
-weigh=np.load("a1000.npy")
-predict=np.load("b1000.npy")
-distinguish=np.load("c1000.npy")
+basefilename="res/40108/"
+inputfilename="data/40108/"
+if os.path.isfile(basefilename+'a'+str(nfeature)+'.npy'):
+    weigh = np.load(basefilename+'a'+str(nfeature)+'.npy')
+else:
+    weigh = np.zeros((nfeature, 2))
+if os.path.isfile(basefilename+'b'+str(nfeature)+'.npy'):
+    predict = np.load(basefilename+'b'+str(nfeature)+'.npy')
+else:
+    predict = np.zeros(nfeature)
+if os.path.isfile(basefilename+'c'+str(nfeature)+'.npy'):
+    distinguish = np.load(basefilename+'c'+str(nfeature)+'.npy')
+else:
+    distinguish = np.zeros((2, nfeature))
+apprearance = np.zeros(nfeature)
 
-img = cv2.imread("2.jpg")
-img1 = cv2.imread("2.jpg")
+
+img = cv2.imread(inputfilename+"0.jpg")
+img1 = cv2.imread(inputfilename+"0.jpg")
 img = cv2.resize(img, (1000, 1000))
 img1 = cv2.resize(img1, (1000, 1000))
 
@@ -103,19 +117,18 @@ kp, res = sift.detectAndCompute(img1, None)
 out = cv2.drawKeypoints(img1, kp, img1)
 cv2.imshow("sift picture of orginal", out)
 
-siftComp(img, 1000)
+siftComp(img, 100)
 
-np.save("a1000", weigh)
-np.save("b1000", predict)
-np.save("c1000", distinguish)
-np.save("d1000", apprearance)
-apprearance_sort=sorted(apprearance,reverse=True)
+np.save(basefilename+'a'+str(nfeature), weigh)
+np.save(basefilename+'b'+str(nfeature), predict)
+np.save(basefilename+'c'+str(nfeature), distinguish)
+np.save(basefilename+'d'+str(nfeature), apprearance)
+apprearance_sort = sorted(apprearance, reverse=True)
 print apprearance_sort
-th=apprearance_sort[50]
-out=img
-for i in range(0,len(kp)):
-    if apprearance[i]>=th:
-        out = cv2.circle(out, (int(kp[i].pt[0]),int(kp[i].pt[1])), 2, (55, 255, 155), 2)
+th = apprearance_sort[50]
+out = img
+for i in range(0, len(kp)):
+    if apprearance[i] >= th:
+        out = cv2.circle(out, (int(kp[i].pt[0]), int(kp[i].pt[1])), 2, (55, 255, 155), 2)
 cv2.imshow("strong sift descriptor", out)
 cv2.waitKey(0)
-
